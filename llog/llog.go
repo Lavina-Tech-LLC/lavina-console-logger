ed6ae5e30f82f2926991fb143d111c130988f149
msiddikov <m.siddikov@gmail.com>
2022-03-20 14:13:56 +0500

    Update llog.go

--- llog/llog.go
+++ llog/llog.go
@@ -3,34 +3,34 @@ package llog
 import "fmt"
 
 const (
-	InfoF    = "\033[1;34m[INFO]\033[0m %s"
-	NoticeF  = "\033[1;36m[Notice]\033[0m %s"
-	WarningF = "\033[1;33m[Warning]\033[0m %s"
-	ErrorF   = "\033[1;31m[Error]\033[0m %s"
-	DebugF   = "\033[0;36m[Debug]\033[0m %s"
+	infoF    = "\033[1;34m[INFO]\033[0m %s"
+	noticeF  = "\033[1;36m[Notice]\033[0m %s"
+	warningF = "\033[1;33m[Warning]\033[0m %s"
+	errorF   = "\033[1;31m[Error]\033[0m %s"
+	debugF   = "\033[0;36m[Debug]\033[0m %s"
 )
 
 func Info(s interface{}) {
-	fmt.Printf(InfoF, s)
+	fmt.Printf(infoF, s)
 	fmt.Println("")
 }
 
 func Notice(s interface{}) {
-	fmt.Printf(NoticeF, s)
+	fmt.Printf(noticeF, s)
 	fmt.Println("")
 }
 
 func Warning(s interface{}) {
-	fmt.Printf(WarningF, s)
+	fmt.Printf(warningF, s)
 	fmt.Println("")
 }
 
 func Error(s interface{}) {
-	fmt.Printf(ErrorF, s)
+	fmt.Printf(errorF, s)
 	fmt.Println("")
 }
 
 func Debug(s interface{}) {
-	fmt.Printf(DebugF, s)
+	fmt.Printf(debugF, s)
 	fmt.Println("")
 }
