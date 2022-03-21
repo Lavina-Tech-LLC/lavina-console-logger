f562d87ba749510e493f8d715ee0456b202170b6
msiddikov <m.siddikov@gmail.com>
2022-03-21 10:43:02 +0500

    added ternary

--- lavina-utils.go
+++ lavina-utils.go
@@ -1,7 +1,15 @@
-package utils
+package lvn
 
 import "fmt"
 
 func Default(s interface{}) {
 	fmt.Println("")
+}
+
+func ternary(c bool, t interface{}, f interface{}) interface{} {
+	if c {
+		return t
+	} else {
+		return f
+	}
 }
